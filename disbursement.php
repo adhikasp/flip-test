ed698e876afe457fd562c409bb045aa05718537e
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2019-07-09 20:55:06 +0700

    Refactor out default curl opt

--- disbursement.php
+++ disbursement.php
@@ -3,38 +3,43 @@ $FLIP_SECRET = getenv("FLIP_SECRET");
 
 class FlipClient {
 
-    private $secret;
+    private $curl_opt;
+
+    private static $DEFAULT_CURL_OPT = array(
+        CURLOPT_URL => "https://nextar.flip.id/disburse",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_HTTPHEADER => array(
+            "Accept: */*",
+            "Cache-Control: no-cache",
+            "Connection: keep-alive",
+            "Content-Type: application/x-www-form-urlencoded",
+            "Host: nextar.flip.id",
+            "accept-encoding: gzip, deflate",
+            "cache-control: no-cache",
+        ),
+        CURLOPT_SSL_VERIFYHOST => 0, // TODO hack to avoid SSL setup
+        CURLOPT_SSL_VERIFYPEER => 0,
+    );
 
     function __construct($secret) {
-        $this->secret = $secret;
+        $this->curl_opt = FlipClient::$DEFAULT_CURL_OPT;
+        array_push($this->curl_opt[CURLOPT_HTTPHEADER], "Authorization: basic " . $secret);
     }
 
-    function postDisbursementRequest() {
+    private function mergeCurlOpt($replacement) {
+        return $this->curl_opt + $replacement;
+    }
+
+    function postDisbursementRequest($bankCode, $accountNumber, $amount, $remark) {
         $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL => "https://nextar.flip.id/disburse",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        curl_setopt_array($ch, $this->mergeCurlOpt(array(
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "bank_code=x&account_number=x&amount=x&remark=x",
-            CURLOPT_HTTPHEADER => array(
-                "Accept: */*",
-                "Authorization: basic " . $this->secret,
-                "Cache-Control: no-cache",
-                "Connection: keep-alive",
-                "Content-Type: application/x-www-form-urlencoded",
-                "Host: nextar.flip.id",
-                "User-Agent: PostmanRuntime/7.15.0",
-                "accept-encoding: gzip, deflate",
-                "cache-control: no-cache",
-                "content-length: 46",
-            ),
-            CURLOPT_SSL_VERIFYHOST => 0, // TODO hack to avoid SSL setup
-            CURLOPT_SSL_VERIFYPEER => 0,
-        ));
+            CURLOPT_POSTFIELDS => "bank_code=${bankCode}&account_number=${accountNumber}&amount=${amount}&remark=${remark}",
+        )));
 
         $response = curl_exec($ch);
         $err = curl_error($ch);
@@ -52,4 +57,4 @@ class FlipClient {
 }
 
 $flipClient = new FlipClient($FLIP_SECRET);
-echo $flipClient->postDisbursementRequest();
+echo $flipClient->postDisbursementRequest("bni", 1234, 1234, "testing");
