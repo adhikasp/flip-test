c561826fe0a711e7badd67b47d4d69eadf718d1f
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2019-07-09 21:10:16 +0700

    Basic database call

--- disbursement.php
+++ disbursement.php
@@ -76,6 +76,51 @@ class FlipClient {
 
 }
 
-$flipClient = new FlipClient($FLIP_SECRET);
-echo $flipClient->postDisbursementRequest("bni", 1234, 1234, "testing");
-echo $flipClient->checkDisbursementStatus(123);
+$flip_client = new FlipClient($FLIP_SECRET);
+echo $flip_client->postDisbursementRequest("bni", 1234, 1234, "testing");
+echo $flip_client->checkDisbursementStatus(123);
+
+// Ref https://www.if-not-true-then-false.com/2012/php-pdo-sqlite3-example/
+$file_db = new PDO('sqlite:disbursement.sqlite3');
+$file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$file_db->exec("CREATE TABLE IF NOT EXISTS disbursement (
+                    transaction_id INTEGER PRIMARY KEY, 
+                    status TEXT, 
+                    receipt TEXT, 
+                    time_served TEXT)");
+
+$dummy_transaction = array(
+    array(
+        'transaction_id' => 1234,
+        'status' => 'PENDING',
+        'receipt' => '',
+        'time_served' => "0000-00-00 00:00:00"),
+    array(
+        'transaction_id' => 1235,
+        'status' => 'SUCCESS!',
+        'receipt' => 'https://flip-receipt.oss-ap-southeast-5.aliyuncs.com/debit_receipt/126316_3d07f9fef9612c7275b3c36f7e1e5762.jpg',
+        'time_served' => "2019-05-21 09:26:11"),
+    );
+
+$insert = "INSERT INTO disbursement (transaction_id, status, receipt, time_served) 
+                VALUES (:transaction_id, :status, :receipt, :time_served)";
+$stmt = $file_db->prepare($insert);
+
+// Bind parameters to statement variables
+$stmt->bindParam(':transaction_id', $transaction_id);
+$stmt->bindParam(':status', $status);
+$stmt->bindParam(':receipt', $receipt);
+$stmt->bindParam(':time_served', $time_served);
+
+// Loop thru all messages and execute prepared insert statement
+foreach ($dummy_transaction as $m) {
+    // Set values to bound variables
+    $transaction_id = $m['transaction_id'];
+    $status = $m['status'];
+    $receipt = $m['receipt'];
+    $time_served = $m['time_served'];
+
+    // Execute statement
+    $stmt->execute();
+}
